57be83f64fd62935e7b5d7e11d281a3d5a862e20
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-07-07 16:31:40 +0200

    controle da conexao do programa com a base de dados

--- src/modelDao/DaoMedico.java
+++ src/modelDao/DaoMedico.java
@@ -29,4 +29,40 @@ public class DaoMedico {
             JOptionPane.showMessageDialog(null, "Os Dados nao foram inseridos!\nErro: " + ex);
         }
     }
+    
+    public ModeloMedico pesquisaMed(ModeloMedico mod){
+        connect.conexao();
+        connect.executaSql("select * from medicos where nome_medico like '%"+mod.getPesquisa()+"%'");
+        try {
+            connect.rs.first();
+            mod.setCod(connect.rs.getInt("idmedico"));
+            mod.setNome(connect.rs.getString("nome_medico"));
+            mod.setEspec(connect.rs.getString("espec_medico"));
+            mod.setBi(connect.rs.getString("bi_medico"));
+            mod.setCrm(connect.rs.getInt("crm_medico"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao fazer a pesquisa:\n"+e.getMessage());
+        }
+        
+        connect.desconectar();
+        return mod;
+    }
+    
+    public void editar(ModeloMedico modelo){
+        connect.conexao();
+        try {
+            PreparedStatement pst = connect.con.prepareStatement("update medicos set nome_medico = ?, espec_medico = ?, crm_medico = ?, bi_medico = ? where idmedico = ?");
+            pst.setString(1, modelo.getNome());
+            pst.setString(2, modelo.getEspec());
+            pst.setInt(3, modelo.getCrm());
+            pst.setString(4, modelo.getBi());
+            pst.setInt(5, modelo.getCod());
+            pst.execute();
+            JOptionPane.showMessageDialog(null, "Dados editados com sucesso!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao editar:\n" + e.getMessage());
+        }
+        connect.desconectar();
+    }
+
 }
