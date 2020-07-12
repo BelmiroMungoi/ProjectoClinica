6b820ccdb60b59075fa8b9cbc01503df0ead10c2
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-07-12 15:35:12 +0200

    getters and setters

---
+++ src/modelBeans/BeansUsuario.java
@@ -0,0 +1,55 @@
+
+package modelBeans;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class BeansUsuario {
+    private Integer usuCod;
+    private String usuNome;
+    private String usuLogin;
+    private String usuSenha;
+    private String usuPerfil;
+
+    public Integer getUsuCod() {
+        return usuCod;
+    }
+
+    public void setUsuCod(Integer usuCod) {
+        this.usuCod = usuCod;
+    }
+
+    public String getUsuNome() {
+        return usuNome;
+    }
+
+    public void setUsuNome(String usuNome) {
+        this.usuNome = usuNome;
+    }
+
+    public String getUsuLogin() {
+        return usuLogin;
+    }
+
+    public void setUsuLogin(String usuLogin) {
+        this.usuLogin = usuLogin;
+    }
+
+    public String getUsuSenha() {
+        return usuSenha;
+    }
+
+    public void setUsuSenha(String usuSenha) {
+        this.usuSenha = usuSenha;
+    }
+
+    public String getUsuPerfil() {
+        return usuPerfil;
+    }
+
+    public void setUsuPerfil(String usuPerfil) {
+        this.usuPerfil = usuPerfil;
+    }
+    
+}
