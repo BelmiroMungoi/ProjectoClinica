813ad1dac052d0658eb4dfd50f37b6369c25bebb
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-07-30 23:43:57 +0200

    script da bd

--- projectoclinica.sql
+++ projectoclinica.sql
@@ -26,4 +26,29 @@ describe usuario;
 select * from usuario;
 delete from usuario where id_usuario = 1;
 
+create table bairro (  
+cod_bairro int primary key auto_increment,
+nome_bairro varchar(50) not null
+);
 
+insert into bairro(nome_bairro) values ('Malhampsene');
+select * from bairro;
+
+create table paciente (
+id_paciente int primary key auto_increment,
+nome_paciente varchar(60) not null,
+nasc_paciente varchar(20) not null,
+bi_paciente varchar(15),
+cell_paciente varchar(20) not null,
+rua_paciente varchar(50),
+quar_paciente varchar(50),
+casa_paciente varchar(50),
+pac_bairro int,
+foreign key(pac_bairro) references bairro(cod_bairro)
+); 
+
+describe paciente;
+select * from paciente;
+delete from paciente where id_paciente = '4';
+
+select id_paciente, nome_paciente, bi_paciente, cell_paciente, nome_bairro, rua_paciente from paciente inner join bairro on pac_bairro = cod_bairro order by nome_paciente;
