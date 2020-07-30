dcefd150e2248d603d53284a99279d4ecc55bba5
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-07-30 23:26:28 +0200

    Getters and Setters

---
+++ src/modelBeans/BeansPaciente.java
@@ -0,0 +1,98 @@
+package modelBeans;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class BeansPaciente {
+    private int cod_pac;
+    private String nome_pac;
+    private String nasc_pac;
+    private String bi_pac;
+    private String cell_pac;
+    private String rua_pac;
+    private String quar_pac;
+    private String casa_pac;
+    private String bairro_pac;
+    private String pesquisa;
+
+    public int getCod_pac() {
+        return cod_pac;
+    }
+
+    public void setCod_pac(int cod_pac) {
+        this.cod_pac = cod_pac;
+    }
+
+    public String getNome_pac() {
+        return nome_pac;
+    }
+
+    public void setNome_pac(String nome_pac) {
+        this.nome_pac = nome_pac;
+    }
+
+    public String getNasc_pac() {
+        return nasc_pac;
+    }
+
+    public void setNasc_pac(String nasc_pac) {
+        this.nasc_pac = nasc_pac;
+    }
+
+    public String getBi_pac() {
+        return bi_pac;
+    }
+
+    public void setBi_pac(String bi_pac) {
+        this.bi_pac = bi_pac;
+    }
+
+    public String getCell_pac() {
+        return cell_pac;
+    }
+
+    public void setCell_pac(String cell_pac) {
+        this.cell_pac = cell_pac;
+    }
+
+    public String getRua_pac() {
+        return rua_pac;
+    }
+
+    public void setRua_pac(String rua_pac) {
+        this.rua_pac = rua_pac;
+    }
+
+    public String getQuar_pac() {
+        return quar_pac;
+    }
+
+    public void setQuar_pac(String quar_pac) {
+        this.quar_pac = quar_pac;
+    }
+
+    public String getCasa_pac() {
+        return casa_pac;
+    }
+
+    public void setCasa_pac(String casa_pac) {
+        this.casa_pac = casa_pac;
+    }
+
+    public String getBairro_pac() {
+        return bairro_pac;
+    }
+
+    public void setBairro_pac(String bairro_pac) {
+        this.bairro_pac = bairro_pac;
+    }
+
+    public String getPesquisa() {
+        return pesquisa;
+    }
+
+    public void setPesquisa(String pesquisa) {
+        this.pesquisa = pesquisa;
+    }
+}
