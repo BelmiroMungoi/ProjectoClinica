ed9153f3e8a8a32b3b822783b190e865f2fca6c9
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2020-08-01 17:17:55 +0200

    Getters and Setters

---
+++ src/modelBeans/BeansEnfermeiro.java
@@ -0,0 +1,63 @@
+
+package modelBeans;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class BeansEnfermeiro {
+   private int cod_enfer;
+   private String nomeEnfer;
+   private String areaEnfer;
+   private String biEnfer;
+   private String cellEnfer;
+   private String pesquisa;
+
+    public int getCod_enfer() {
+        return cod_enfer;
+    }
+
+    public void setCod_enfer(int cod_enfer) {
+        this.cod_enfer = cod_enfer;
+    }
+
+    public String getNomeEnfer() {
+        return nomeEnfer;
+    }
+
+    public void setNomeEnfer(String nomeEnfer) {
+        this.nomeEnfer = nomeEnfer;
+    }
+
+    public String getAreaEnfer() {
+        return areaEnfer;
+    }
+
+    public void setAreaEnfer(String areaEnfer) {
+        this.areaEnfer = areaEnfer;
+    }
+
+    public String getBiEnfer() {
+        return biEnfer;
+    }
+
+    public void setBiEnfer(String biEnfer) {
+        this.biEnfer = biEnfer;
+    }
+
+    public String getCellEnfer() {
+        return cellEnfer;
+    }
+
+    public void setCellEnfer(String cellEnfer) {
+        this.cellEnfer = cellEnfer;
+    }
+
+    public String getPesquisa() {
+        return pesquisa;
+    }
+
+    public void setPesquisa(String pesquisa) {
+        this.pesquisa = pesquisa;
+    }
+}
