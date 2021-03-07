2b7cdd38e8ff3e228eebcd761ecd1361ef4e31f9
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-07 20:05:17 +0200

    Getters e Setters da consulta

---
+++ src/modelBeans/BeansConsulta.java
@@ -0,0 +1,45 @@
+
+package modelBeans;
+
+/**
+ *
+ * @author Belmiro-Mungoi
+ */
+public class BeansConsulta {
+    private int codConsulta;
+    private String diaConsulta;
+    private String receConsulta;
+    private String nomePac;
+
+    public int getCodConsulta() {
+        return codConsulta;
+    }
+
+    public void setCodConsulta(int codConsulta) {
+        this.codConsulta = codConsulta;
+    }
+
+    public String getDiaConsulta() {
+        return diaConsulta;
+    }
+
+    public void setDiaConsulta(String diaConsulta) {
+        this.diaConsulta = diaConsulta;
+    }
+
+    public String getReceConsulta() {
+        return receConsulta;
+    }
+
+    public void setReceConsulta(String receConsulta) {
+        this.receConsulta = receConsulta;
+    }
+
+    public String getNomePac() {
+        return nomePac;
+    }
+
+    public void setNomePac(String nomePac) {
+        this.nomePac = nomePac;
+    }
+}
