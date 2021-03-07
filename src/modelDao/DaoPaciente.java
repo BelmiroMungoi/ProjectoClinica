a77a0734e6de407b1646fdd219c79236e32daf01
Belmiro Mungoi <56709665+BelmiroMungoi@users.noreply.github.com>
2021-03-07 20:02:47 +0200

    Crud para paciente

--- src/modelDao/DaoPaciente.java
+++ src/modelDao/DaoPaciente.java
@@ -2,8 +2,6 @@ package modelDao;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import modelBeans.BeansPaciente;
 import modelConexao.ConexaoDb;
